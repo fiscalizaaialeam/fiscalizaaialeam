049d10047ddd4faf2f0ba43915a14ea61fbaa0ce
fiscalizaaialeam <123780408+fiscalizaaialeam@users.noreply.github.com>
2023-02-11 15:18:44 -0400

    Mudança de nome referente a imagem

--- pages/ranking/ranking.js
+++ pages/ranking/ranking.js
@@ -18,7 +18,7 @@ for (let a = 23; a >= 0; a--) {
     // imagem 
     num_ranking -= 1;
     img = document.createElement("img");
-    img.src = `../../images/deputados/${rankingDataOrdered[a].urlfoto}-min.png`;
+    img.src = `../../images/deputados/${rankingDataOrdered[a].foto}-min.png`;
     // nome do deputado
     let name = document.createElement("h2");
     name.innerHTML = `${num_ranking}° - ${rankingDataOrdered[a].nome}`;
